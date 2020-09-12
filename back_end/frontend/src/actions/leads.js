d82a1a9b0ac669a6035884315f0e81ed435fee04
Brian Cummins <b.cummins111@gmail.com>
2020-09-12 19:19:41 +0100

    added error handling and alerts

--- back_end/frontend/src/actions/leads.js
+++ back_end/frontend/src/actions/leads.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
+import { createMessage, returnErrors } from "./messages";
+import { createStore } from "redux";
+
 // GET LEADS
 export const getLeads = () => (dispatch) => {
   axios
@@ -21,6 +24,7 @@ export const deleteLead = (id) => (dispatch) => {
   axios
     .delete(`/api/leads/${id}/`)
     .then((res) => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEAD,
         payload: id,
@@ -34,10 +38,13 @@ export const addLead = (lead) => (dispatch) => {
   axios
     .post("/api/leads/", lead)
     .then((res) => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
